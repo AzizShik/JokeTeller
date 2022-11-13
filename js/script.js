c6c0749137a0e5e84eb3fcf6c27fcc2734dddc87
Abdyl-Aziz <azizshihabov@mail.ru>
2022-11-13 23:04:40 +0300

    feat: add some functionality

--- js/script.js
+++ js/script.js
@@ -1,41 +1,73 @@
 window.addEventListener('load', () => {
 	const jokeEl = document.querySelector('.joke');
-	const tts = window.speechSynthesis;
+	const jokeContainerEl = document.querySelector('.joke__container');
+	const jokeProgressEl = document.querySelector('.joke__progress');
 	const btnEl = document.querySelector('.btn');
+	const API_KEY = '5134f0b021ab45e9a73eb60c2310f1ba';
 
 	async function getJoke() {
-		const res = await fetch(
-			'https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,racist,sexist,explicit'
-		);
-		const data = await res.json();
-		return data;
+		try {
+			const res = await fetch(
+				'https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,racist,sexist,explicit'
+			);
+			const data = await res.json();
+			return data;
+		} catch (error) {
+			console.error(error);
+		}
 	}
 
 	async function loadJoke() {
-		jokeEl.classList.add('joke--active');
-		jokeEl.innerHTML = '';
-		const data = await getJoke();
+		try {
+			jokeEl.classList.add('joke--active');
+			jokeContainerEl.innerHTML = '';
+			const data = await getJoke();
+			let jokeText;
 
-		if (data.type === 'single') {
-			jokeEl.innerHTML = `${data.joke}`;
-			convertTextToSpeech(data.joke);
-		}
+			if (data.type === 'single') {
+				jokeContainerEl.innerHTML = `${data.joke}`;
+				jokeText = `${data.joke}`;
+			}
 
-		if (data.type === 'twopart') {
-			jokeEl.innerHTML = `${data.setup} <br> ${data.delivery}`;
-			const text = `${data.setup} ${data.delivery}`;
-			convertTextToSpeech(text);
+			if (data.type === 'twopart') {
+				jokeContainerEl.innerHTML = `${data.setup} <br> ${data.delivery}`;
+				jokeText = `${data.setup} ${data.delivery}`;
+			}
+
+			VoiceRSS.speech({
+				key: API_KEY,
+				src: jokeText,
+				hl: 'en-us',
+				v: 'Linda',
+				r: 0,
+				c: 'mp3',
+				f: '44khz_16bit_stereo',
+				ssml: false,
+			});
+		} catch (error) {
+			console.error(error);
 		}
 	}
 
-	function convertTextToSpeech(text) {
-		const utterance = new SpeechSynthesisUtterance(text.trim());
-		utterance.voice = tts.getVoices()[3];
-		utterance.picth = 1;
-		utterance.rate = 1;
-		tts.speak(utterance);
-	}
+	btnEl.addEventListener('click', () => {
+		jokeEl.classList.remove('joke-time');
+		loadJoke();
+	});
 
-	btnEl.addEventListener('click', loadJoke);
+	audioElement.addEventListener('play', function () {
+		setTimeout(() => {
+			btnEl.disabled = true;
+			jokeEl.classList.add('joke-time');
+			jokeProgressEl.style.animationDuration = +audioElement.duration + 5 + 's';
+		}, 200);
+	});
+
+	audioElement.addEventListener('ended', () => {
+		btnEl.disabled = false;
+		setTimeout(() => {
+			jokeEl.classList.remove('joke-time');
+		}, 5000);
+	});
+
 	loadJoke();
 });
